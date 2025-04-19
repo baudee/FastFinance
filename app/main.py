1cd5ba6ebbd02eef5ddf40a9030e08e82b48be22
Baudin Valentin <valentin.baudin@hes-so.ch>
2025-04-19 11:27:07 +0200

    feat: search, ticker details and currency change

--- app/main.py
+++ app/main.py
@@ -1,15 +1,35 @@
-from typing import Union
+from fastapi import FastAPI, HTTPException
+import yfinance as yf
 
-from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def get_ticker_info(ticker_symbol: str):
+    try:
+        ticker = yf.Ticker(ticker_symbol)
+        info = ticker.info
+        if not info or not info.get("regularMarketPrice"):
+            raise Exception("Ticker not found")
+        return info
+    except Exception:
+        raise HTTPException(
+            status_code=404, detail=f"Ticker not found: {ticker_symbol}"
+        )
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/ticker/search")
+def search(symbol: str, limit: int = 10):
+    data = yf.Search(symbol, max_results=limit)
+    return data.quotes
+
+
+@app.get("/ticker/{symbol}")
+def details(symbol: str):
+    return get_ticker_info(symbol)
+
+
+@app.get("/currency/{from_currency}/{to_currency}")
+def currency(from_currency: str, to_currency: str):
+    symbol = f"{from_currency.upper()}{to_currency.upper()}=X"
+    return get_ticker_info(symbol)
